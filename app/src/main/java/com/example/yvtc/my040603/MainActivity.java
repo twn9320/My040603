92c53938fecd59e7f29eb2e55baa871fdff70c83
twn9320 <twn9320@gmail.com>
2017-04-06 16:12:04 +0800

    下載圖片

--- app/src/main/java/com/example/yvtc/my040603/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my040603/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.yvtc.my040603;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -17,36 +21,41 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     InputStream inputStream;
     TextView tv;
+    ByteArrayOutputStream os;
+    byte[] buffer;
+    ImageView img;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView)findViewById(R.id.textView);
 
+        os = new ByteArrayOutputStream();
+        img = (ImageView)findViewById(R.id.imageView);
+        buffer = new byte[64];
         new Thread(){
             @Override
             public void run() {
                 super.run();
                 try {
-                    URL url = new URL("https://udn.com/rssfeed/news/1");
+                    URL url = new URL("https://uc.udn.com.tw/photo/2017/04/06/1/3364635.jpg");
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setRequestMethod("GET");
                     conn.connect();
                     inputStream = conn.getInputStream();
 
-                    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-                    final StringBuilder sb = new StringBuilder();
-                    String str;
-                    while ((str = br.readLine()) != null)
-                    {
-                        sb.append(str);
+                    int readSize = 0;
+                    while ((readSize=inputStream.read(buffer)) != -1) {
+                        os.write(buffer, 0, readSize);
                     }
-                    Log.d("MYNETLog", sb.toString());
+                    byte[] result = os.toByteArray();
+                    final Bitmap bitmap = BitmapFactory.decodeByteArray(result,0,result.length);
 
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            tv.setText(sb.toString());
+                           img.setImageBitmap(bitmap);
                         }
                     });
 
