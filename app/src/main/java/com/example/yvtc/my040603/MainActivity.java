28f99561dde2c1560a081de3741523a577015eda
twn9320 <twn9320@gmail.com>
2017-04-06 15:34:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/my040603/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.yvtc.my040603;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    InputStream inputStream;
+    Handler handler;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.textView);
+        handler = new Handler();
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    URL url = new URL("https://udn.com/rssfeed/news/1");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    inputStream = conn.getInputStream();
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                    final StringBuilder sb = new StringBuilder();
+                    String str;
+                    while ((str = br.readLine()) != null)
+                    {
+                        sb.append(str);
+                    }
+                    Log.d("MYNETLog", sb.toString());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv.setText(sb.toString());
+                        }
+                    });
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+    }
+}
